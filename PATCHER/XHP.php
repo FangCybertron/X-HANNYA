5a052ff23cb916f4e5554f3f1cbf12966bf66ca7
Miss Fang <41809800+missfangg@users.noreply.github.com>
2021-07-29 00:11:25 +0700

    Update XHP.php
    Update

--- PATCHER/XHP.php
+++ PATCHER/XHP.php
@@ -2225,7 +2225,7 @@ function LoginExpired()
   Exp2()
  
   function expired()
-    Date1 = "20210728"--Expired Date
+    Date1 = "20210729"--Expired Date
     Date2 = "%Y%m%d"--Will be show if the date has changed to less than the current date set.
     Date3 = "20210727"--Current Date
     date = os.date("%Y%m%d")
