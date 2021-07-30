24048f1f41b1ceb60ca0b446c53fafef8a3642f0
Miss Fang <41809800+missfangg@users.noreply.github.com>
2021-07-30 16:52:34 +0700

    Add files via upload

--- PATCHER/SERVER.php
+++ PATCHER/SERVER.php
@@ -13,23 +13,6 @@ import "android.content.pm.PackageManager"
 import "android.graphics.PixelFormat"
 import "android.graphics.Typeface"
 import "android.content.Context"
-  require "import"
-  import "android.app.*"
-  import "android.os.*"
-  import "android.widget.*"
-  import "android.view.*"
-  import "android.graphics.drawable.BitmapDrawable"
-  import "android.graphics.PorterDuff"
-  import "android.graphics.PorterDuffColorFilter"
-  import "android.content.Context"
-  import "android.content.Intent"
-  import "android.net.Uri"
-  import "android.provider.Settings"
-  import "com.androlua.util.RootUtil"
-  import "android.graphics.Typeface"
-  local root=RootUtil()
-
-
 
 
 
@@ -379,9 +362,27 @@ rememberme.setVisibility(LinearLayout.VISIBLE)
 
 function main2()
 
+  require "import"
+  import "android.app.*"
+  import "android.os.*"
+  import "android.widget.*"
+  import "android.view.*"
+  import "android.graphics.drawable.BitmapDrawable"
+  import "android.graphics.PorterDuff"
+  import "android.graphics.PorterDuffColorFilter"
+  import "android.content.Context"
+  import "android.content.Intent"
+  import "android.net.Uri"
+  import "android.provider.Settings"
+  import "com.androlua.util.RootUtil"
+  import "android.graphics.Typeface"
+  local root=RootUtil()
+
+
   if Settings.canDrawOverlays(activity) then else intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,Uri.parse("package:".. activity.getPackageName()))
     intent.setData(Uri.parse("package:" .. this.getPackageName())); this.startActivity(intent);
     TOASTTXT("Application Require Permission")
+    return
   end
 
   os.execute("mkdir /storage/emulated/0/XHP-PROJECT")
